53bf5ad4bd176420fc6dd13c9566073f0eb57f9b
haruby2357 <haruby2357@gmail.com>
2026-01-24 00:31:24 +0900

    [feat] 알림 UI 기본 틀

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -6,11 +6,10 @@ import ReviewWrite from "../features/comm/ReviewWrite";
 import CommMain from "../features/comm/CommMain";
 import PostDetail from "../features/comm/PostDetail";
 import MyMain from "../features/myPage/MyMain";
+import Notifications from "../features/alerts/Notifications";
 
 export default function AppRouter() {
   return (
-    // Nav바 동작 확인을 위한 임시 라우팅
-    // 추후 개별 페이지 구현 시 교체 예정
     <Routes>
       <Route path="/" element={<Home />} />
 
@@ -26,6 +25,9 @@ export default function AppRouter() {
       {/* 개인 페이지 라우팅 */}
       <Route path="/heart" element={<Home />} />
       <Route path="/mypage" element={<MyMain />} />
+      <Route path="/notifications" element={<Notifications />} />
+
     </Routes>
   );
 }
+
