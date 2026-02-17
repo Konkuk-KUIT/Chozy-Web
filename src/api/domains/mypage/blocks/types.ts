20c21049ae5607edea20f5b33100f4ad50822fef
dododokk <kdoeun05@konkuk.ac.kr>
2026-02-17 22:23:42 +0900

    [fix] CI 문제 해결

--- src/api/domains/mypage/blocks/types.ts
+++ src/api/domains/mypage/blocks/types.ts
@@ -1,5 +1,4 @@
 // 마이페이지 차단한 계정 목록 조회
-
 export type BlockedAccountItem = {
   userId: number;
   loginId: string;
@@ -15,4 +14,11 @@ export type BlockedAccountsResult = {
   totalElements: number;
   totalPages: number;
   hasNext: boolean;
+};
+
+// 마이페이지 차단 해제
+export type UnblockResult = {
+  targetUserId: number;
+  isBlocked: false; // 성공 시 false
+  unblockedAt: string;
 };
